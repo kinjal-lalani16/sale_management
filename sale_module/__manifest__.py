21a34d490da37b2f774b4991e734cbc2e86d91f2
Kinal <kinjal.lalani@icreativetechnolabs.com>
2020-11-30 10:46:27 +0530

    [init]Initial Commit

---
+++ sale_module/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sale",
+    'summary': """This module will add extra field in sale line""",
+    'description': """This module will add extra field in sale line""",
+    'author': "Aktiv software",
+    'website': "http://www.aktivesoftware.com",
+    'category': 'Tools',
+    'version': '13.0.1.0.0',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
